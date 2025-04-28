1cd697dc1b771ef4acb22ad7c878daf4f9110b7c
MinboyKim <kdm3801@gmail.com>
2025-04-28 15:45:11 +0900

    feat: Toaster
    Toaster from sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthInitializer } from "@/components/auth-initializer";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        <AuthInitializer />
         {children}
+        <AuthInitializer />
+        <Toaster />
       </body>
     </html>
   );
