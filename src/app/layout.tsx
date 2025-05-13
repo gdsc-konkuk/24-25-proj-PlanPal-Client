c7c4a7d8bc91f2e871b3944664de36d21a59b069
kim jihwan <kjhwan0802@naver.com>
2025-05-13 11:32:22 +0900

    feat: update metadata for SEO and improve site description; add Open Graph and Twitter card data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { Header } from "@/components/global/header";
 import { Footer } from "@/components/global/footer";
 import { usePathname } from "next/navigation";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,8 +19,54 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "PlanPal",
-  description: "Map, Chat, Go",
+  metadataBase: new URL("https://planpal.site"),
+  title: "PlanPal - Map, Chat, Go",
+  keywords: [
+    "PlanPal",
+    "Map",
+    "Chat",
+    "Go",
+    "Planning",
+    "Travel",
+    "Group",
+    "Chat",
+    "Group Chat",
+    "Group AI Chat",
+    "AI",
+    "AI Chat",
+    "Map",
+  ],
+  description: "PlanPal is a group planning tool that helps you map, chat, and go. It allows you to create a map with your friends, chat with them, and plan your trip together.",
+  authors: [
+    {
+      name: "GDG on Campus Konkuk",
+      url: "https://gdg.community.dev/gdg-on-campus-konkuk-university-seoul-south-korea/",
+    },
+  ],
+  creator: "GDG on Campus Konkuk",
+  publisher: "GDG on Campus Konkuk",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
+  openGraph: {
+    title: "PlanPal",
+    description: "Map, Chat, Go",
+    url: "https://planpal.site",
+    siteName: "PlanPal",
+    images: "/logo.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "PlanPal",
+    description: "Map, Chat, Go",
+    images: "/logo.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
